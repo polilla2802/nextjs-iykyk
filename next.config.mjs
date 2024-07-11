406eac773e16f3cf15dc2908b7c8fecd167380b4
Jose Manuel Alarcon <polilla2802@gmail.com>
2024-07-11 01:54:10 -0500

    added firebase storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from "dotenv";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+const nextConfig = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    APP_ENV: process.env.APP_ENV,
+    NEXT_PORT: process.env.NEXT_PORT,
+    NEXT_FIREBASE_API_KEY: process.env.NEXT_FIREBASE_API_KEY,
+    NEXT_FIREBASE_AUTH_DOMAIN: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+    NEXT_FIREBASE_PROJECT_ID: process.env.NEXT_FIREBASE_PROJECT_ID,
+    NEXT_FIREBASE_STORAGE_BUCKET: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
+    NEXT_FIREBASE_MESSAGING_SENDER_ID:
+      process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_FIREBASE_APP_ID: process.env.NEXT_FIREBASE_APP_ID,
+    NEXT_MEASUREMENT_ID: process.env.NEXT_MEASUREMENT_ID,
+  },
+};
 
 export default nextConfig;
