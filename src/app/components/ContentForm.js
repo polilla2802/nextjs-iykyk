75178175f454e6d0415978864a77fae191915462
Webmaster Tachuela <78869984+polilla2802@users.noreply.github.com>
2024-07-29 12:38:01 -0500

    Export pdf

--- src/app/components/ContentForm.js
+++ src/app/components/ContentForm.js
@@ -1,11 +1,9 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
-import dynamic from 'next/dynamic';
 import styles from "./ContentForm.module.css"; // Import the CSS module
-
-// Dynamically import html2canvas and html2pdf
-const html2canvas = dynamic(() => import("html2canvas"), { ssr: false });
-const html2pdf = dynamic(() => import("html2pdf.js"), { ssr: false });
+import toast from "react-hot-toast";
+import { useState, useRef, useEffect } from "react";
+import html2canvas from "html2canvas";
+import html2pdf from "html2pdf.js";
 
 export default function ContentForm() {
 	const [isMounted, setIsMounted] = useState(false);
@@ -18,10 +16,19 @@ export default function ContentForm() {
 		MEMBERSHIPTYPE: "",
 		BIRTHDAY: "",
 		DOCUMENTID: "",
+		SOCIOCODE: ""
 	});
+	const [socioCode, setSocioCode] = useState("");
 
 	const tableRef = useRef();
-
+	const generateSocioCode = () => {
+		const code = Math.floor(Math.random() * 10 ** 13).toString().padStart(13, '0');
+		setSocioCode(code);
+		setFormData({
+			...formData,
+			SOCIOCODE: code,
+		});
+	};
 	const handleCapture = async () => {
 		const table = tableRef.current;
 
@@ -42,8 +49,8 @@ export default function ContentForm() {
 
 		const options = {
 			margin: 10,
-			filename: "documento.pdf",
-			image: { type: "jpeg", quality: 0.98 },
+			filename: "IYKYK.pdf",
+			image: { type: "png", quality: 1 },
 			html2canvas: { scale: 3 },
 			jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
 		};
@@ -62,6 +69,8 @@ export default function ContentForm() {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		console.log(formData);
+		console.log("holi")
 		if (
 			!formData.NAME ||
 			!formData.MEMBERSHIPTYPE ||
@@ -89,15 +98,17 @@ export default function ContentForm() {
 				", ",
 			)}.`;
 
-			console.log(mensaje)
+			toast.error(mensaje);
 			return;
+		} else {
+			data.formData = formData;
+			await handleOpenPDF();
 		}
-		await handleCapture();
-		data.formData = formData;
 	};
 
 	useEffect(() => {
 		setIsMounted(true);
+		generateSocioCode();
 	}, []);
 
 	if (!isMounted) {
@@ -105,154 +116,160 @@ export default function ContentForm() {
 	}
 
 	return (
-		<div className="grid-cols-2 lg:grid justify-items-center mt-10">
-			<div className="w-full mx-auto p-2 mb-10 bg-white rounded-lg shadow-2xl md:w-2/3">
-				<div className="w-full bg-white">
-					<form
-						className="px-8 pt-6 pb-8 mb-4 bg-white"
-						onSubmit={handleSubmit}
-						ref={form}
-					>
-						<>
-							<label
-								htmlFor="NAME"
-								className="block mb-2 text-sm font-bold text-primaryBlue"
-							>
-								NOMBRE:
-								<i className="font-light">
-									<span className="text-red-700"> *</span>
-								</i>
-							</label>
-							<input
-								name="NAME"
-								type="text"
-								onChange={handleChange}
-								maxLength={18}
-								className="w-full px-3 py-2 border shadow appearance-none"
-								required
-								autoFocus
-							/>
-						</>
+		<form
 
-						<>
-							<label
-								htmlFor="MEMBERSHIPTYPE"
-								className="block my-2 text-sm font-bold text-primaryBlue"
-							>
-								MEMBRESIA:
-								<i className="font-light">
-									<span className="text-red-700"> *</span>
-								</i>
-							</label>
-
-							<select
-								name="MEMBERSHIPTYPE"
-								onChange={handleChange}
-								className="w-full px-3 py-2 border shadow"
-								required
-							>
-								<option value="">Seleccionar</option>
-								<option value="ANUAL">ANUAL</option>
-								<option value="IYKYK">IYKYK</option>
-								<option value="MENSUAL">MENSUAL</option>
-							</select>
-						</>
-
-						<>
-							<label
-								htmlFor="BIRTHDAY"
-								className="block my-2 text-sm font-bold text-primaryBlue"
-							>
-								CUMPLEAÑOS:
-								<i className="font-light">
-									<span className="text-red-700"> *</span>
-								</i>
-							</label>
-							<input
-								name="BIRTHDAY"
-								type="date"
-								onChange={handleChange}
-								className="w-full px-3 py-2 border shadow appearance-none"
-								required
-							/>
-						</>
-
-						<>
-							<label
-								htmlFor="DOCUMENTID"
-								className="block my-2 text-sm font-bold text-primaryBlue"
-							>
-								NÚMERO DE DOCUMENTO:
-								<i className="font-light">
-									<span className="text-red-700"> *</span>
-								</i>
-							</label>
-							<input
-								name="DOCUMENTID"
-								type="text"
-								maxLength={20}
-								onChange={handleChange}
-								className="w-full px-3 py-2 border shadow appearance-none"
-								required
-							/>
-						</>
-					</form>
-				</div>
-			</div>
-			<div>
-				<div className={`${styles.membership}`} ref={tableRef} id="tarjeta">
-					<>
-						<table
-							background={"/images/membership-final.png"}
-							className="bg-no-repeat bg-cover bg-center rounded-2xl"
+			onSubmit={handleSubmit}
+			ref={form}
+		>
+			<div className="grid-cols-2 mt-10 lg:grid justify-items-center">
+				<div className="w-full p-2 mx-auto mb-10 bg-white rounded-lg shadow-2xl lg:w-4/5">
+					<div className="w-full bg-white">
+						<div
+							className="px-8 pt-6 pb-8 mb-4 bg-white"
 						>
-							<tbody height="100%">
-								<tr height="100%">
-									<td width="60%" className="align-bottom pb-5 pl-5 md:pl-10">
-										<div>
-											<p className="py-0 my-0 leading-4 text-left capitalize">
-												{formData.NAME !== "" ? formData.NAME : "NOMBRE"}
-											</p>
-											<p className="py-0 my-0 leading-4 text-left capitalize">
-												{formData.MEMBERSHIPTYPE !== "" ? formData.MEMBERSHIPTYPE : "MEMBRESIA"}
-											</p>
-											<p className="py-0 my-0 leading-4 text-left capitalize">
-												{formData.BIRTHDAY !== "" ? formData.BIRTHDAY : "CUMPLEAÑOS"}
-											</p>
-											<p className="py-0 my-0 leading-4 text-left capitalize">
-												{formData.DOCUMENTID !== "" ? formData.DOCUMENTID : "NÚMERO DE DOCUMENTO"}
-											</p>
-										</div>
-									</td>
-									<td width="40%" className={`${styles.socio} align-bottom pb-5 pr-5`}>
-										<div>
-											<p className="py-0 text-left capitalize whitespace-nowrap">
-												CÓDIGO DE SOCIO
-											</p>
-											<p className="py-0 text-left capitalize">
-												0000000000000
-											</p>
-										</div>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</>
+							<>
+								<label
+									htmlFor="NAME"
+									className="block mb-2 text-sm font-bold text-primaryBlue"
+								>
+									NOMBRE:
+									<i className="font-light">
+										<span className="text-red-700"> *</span>
+									</i>
+								</label>
+								<input
+									name="NAME"
+									type="text"
+									onChange={handleChange}
+									maxLength={18}
+									className="w-full px-3 py-2 border shadow appearance-none"
+									required
+									autoFocus
+								/>
+							</>
+
+							<>
+								<label
+									htmlFor="MEMBERSHIPTYPE"
+									className="block my-2 text-sm font-bold text-primaryBlue"
+								>
+									MEMBRESIA:
+									<i className="font-light">
+										<span className="text-red-700"> *</span>
+									</i>
+								</label>
+
+								<select
+									name="MEMBERSHIPTYPE"
+									onChange={handleChange}
+									className="w-full px-3 py-2 border shadow"
+									required
+								>
+									<option value="">Seleccionar</option>
+									<option value="ANUAL">ANUAL</option>
+									<option value="IYKYK">IYKYK</option>
+									<option value="MENSUAL">MENSUAL</option>
+								</select>
+							</>
+
+							<>
+								<label
+									htmlFor="BIRTHDAY"
+									className="block my-2 text-sm font-bold text-primaryBlue"
+								>
+									CUMPLEAÑOS:
+									<i className="font-light">
+										<span className="text-red-700"> *</span>
+									</i>
+								</label>
+								<input
+									name="BIRTHDAY"
+									type="date"
+									onChange={handleChange}
+									className="w-full px-3 py-2 border shadow appearance-none"
+									required
+								/>
+							</>
+
+							<>
+								<label
+									htmlFor="DOCUMENTID"
+									className="block my-2 text-sm font-bold text-primaryBlue"
+								>
+									NÚMERO DE DOCUMENTO:
+									<i className="font-light">
+										<span className="text-red-700"> *</span>
+									</i>
+								</label>
+								<input
+									name="DOCUMENTID"
+									type="text"
+									maxLength={20}
+									onChange={handleChange}
+									className="w-full px-3 py-2 border shadow appearance-none"
+									required
+								/>
+							</>
+						</div>
+					</div>
 				</div>
-				<p className="pt-5 text-center">
-					Esta representación es lo mas cercano posible a la tarjeta final
-				</p>
-				<div className="flex flex-col items-center mt-6">
-					<div className="w-full mb-4">
-						<button
-							onClick={handleOpenPDF}
-							className="block uppercase whitespace-nowrap font-formaBold tracking-widest w-min m-auto px-4 py-2 text-xs text-center text-white shadow bg-primaryBlue md:text-sm"
-						>
-							Generar PDF
-						</button>
+				<div className="w-full">
+					<div className={`${styles.membership}`} ref={tableRef} id="tarjeta">
+						<>
+							<table
+								background={"/images/membership-final.png"}
+								className="bg-center bg-no-repeat bg-cover rounded-2xl"
+							>
+								<tbody height="100%">
+									<tr height="100%">
+										<td width="60%" className="pb-8 pl-5 align-bottom md:pl-10">
+											<div>
+												<p className="py-0 my-0 leading-4 text-left capitalize">
+													{formData.NAME !== "" ? formData.NAME : "NOMBRE"}
+												</p>
+												<p className="py-0 my-0 leading-4 text-left capitalize">
+													{formData.MEMBERSHIPTYPE !== "" ? formData.MEMBERSHIPTYPE : "MEMBRESIA"}
+												</p>
+												<p className="py-0 my-0 leading-4 text-left capitalize">
+													{formData.BIRTHDAY !== "" ? formData.BIRTHDAY : "CUMPLEAÑOS"}
+												</p>
+												<p className="py-0 my-0 leading-4 text-left capitalize">
+													NÚMERO DE DOCUMENTO
+												</p>
+												<p className="py-0 my-0 leading-4 text-left capitalize">
+													{formData.DOCUMENTID !== "" ? formData.DOCUMENTID : "XXXXXXXXXXX"}
+												</p>
+											</div>
+										</td>
+										<td width="40%" className={`${styles.socio} align-bottom pb-8 pr-5`}>
+											<div>
+												<p className="py-0 text-left capitalize whitespace-nowrap">
+													CÓDIGO DE SOCIO
+												</p>
+												<p className="py-0 text-left capitalize">
+													{socioCode}
+												</p>
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</>
+					</div>
+					<p className="pt-5 text-center">
+						Esta representación es lo mas cercano posible a la tarjeta final
+					</p>
+					<div className="flex flex-col items-center mt-6">
+						<div className="w-full mb-4">
+							<button
+								className="block px-4 py-2 m-auto text-xs tracking-widest text-center text-white uppercase shadow whitespace-nowrap font-formaBold w-min bg-primaryBlue md:text-sm"
+							>
+								Generar PDF
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</form>
 	);
 }
