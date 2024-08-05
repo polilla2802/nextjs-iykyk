2f5a1b4f61f4e0e5c105acb04dadcfb76661dd23
Webmaster Tachuela <78869984+polilla2802@users.noreply.github.com>
2024-08-05 10:43:00 -0500

    removed documentId as param

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="container px-4 py-4 m-auto">
         <h1 className="text-8xl">IYKYK</h1>
         <UpoloadDocument />
-        <ContentForm documentId={"990824805647122433"} />
+        <ContentForm />
       </section>
     </>
   );
